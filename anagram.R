46d1ba2a6e00c34f4921df97ad97c2f0393a1e5e
jerzyt <buyer@jtomasik.com>
2013-12-05 00:42:30 -0800

    anagram.R: removed printing of intermediate times

--- anagram.R
+++ anagram.R
@@ -1,10 +1,6 @@
 #	1.	Import the dictionary file
 startTime <- proc.time()['elapsed']
 dic <- read.csv('words', header=FALSE)
-endTime <- proc.time()['elapsed']
-elapsed <- endTime - startTime 
-startTime <- endTime
-print(elapsed)
 
 #	2.	Create a column with sorted letters
 key <- function(str) {
@@ -12,26 +8,14 @@ key <- function(str) {
 }
 
 keys <- apply(dic, 1, key)
-endTime <- proc.time()['elapsed']
-elapsed <- endTime - startTime 
-startTime <- endTime
-print(elapsed)
 
 #	3.	Combine dic with keys
 dic <- cbind(dic, keys)
 names(dic) <- c('word', 'key')
-endTime <- proc.time()['elapsed']
-elapsed <- endTime - startTime 
-startTime <- endTime
-print(elapsed)
 
 #	4.	Find keys with > 3 occurences
 counts <- as.data.frame(table(dic$key))
 anagramKeys <- subset(counts, select=(Var1), subset=(Freq > 3))
-endTime <- proc.time()['elapsed']
-elapsed <- endTime - startTime 
-startTime <- endTime
-print(elapsed)
 
 #	5.	Print the anagrams
 printAnagrams <- function(aKey) {
@@ -42,5 +26,4 @@ printAnagrams <- function(aKey) {
 apply(anagramKeys, 1, printAnagrams)
 endTime <- proc.time()['elapsed']
 elapsed <- endTime - startTime 
-startTime <- endTime
 print(elapsed)
